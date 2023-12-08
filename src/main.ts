7cdd4b1cb9592a6c056b759c23ce80a77ab28aad
Aleks Zavyalov <zavyalov.aleks@gmail.com>
2023-12-08 16:39:34 +0300

    Add cors origin for FE

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:4200', 'https://fc-cvw.xyz'],
+    origin: ['http://localhost:4200', 'https://hos-test.vercel.app'],
   });
   app.setGlobalPrefix('hos-be')
   await app.listen(3000);
